9e87b4d9b8c51bf1ca83a380754597eee0ed6558
thomasjball <thomasjball@users.noreply.github.com>
2025-04-26 19:44:19 +0000

    deploy: 29d86de6bbd5fd58a7999ee9145e8decb8353d15

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "811424c9180c665bcb573b3a639683a2"
+    "revision": "f978ab587ab8f962e2bc75713cdd975c"
   },
   {
     "url": "static/webfonts/s/roboto/v47/KFOMCnqEu92Fr1ME7kSn66aGLdTylUAMQXC89YmC2DPNWuaabVmUiAo.woff2"
@@ -160,7 +160,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/simx/jacdac/pxt-jacdac/-/app-f8c8d7c2f5a745fc9f1f.js`))) {
+  if (!resources || !(await caches.match(`/simx/jacdac/pxt-jacdac/-/app-f7812d834e441d4feb4d.js`))) {
     return await fetch(event.request)
   }
 
