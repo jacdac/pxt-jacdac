7ba58aa969c3a31204fe05a4c1e7b92ce08b2448
Aron Eggens <aroneggens@gmail.com>
2026-05-22 14:00:34 +0100

    Revert "4 additional LED clients for using up to 8 LEDs"
    This reverts commit f3c44b2ddb92b7ffeb35a5131e9f29f1c3df12d4.

--- led/clients.ts
+++ led/clients.ts
@@ -5,12 +5,4 @@ namespace modules {
     export const led3 = new LedClient("led 3")
     //% fixedInstance whenUsed weight=4 block="led 4"
     export const led4 = new LedClient("led 4")
-    //% fixedInstance whenUsed weight=4 block="led 5"
-    export const led5 = new LedClient("led 5")
-    //% fixedInstance whenUsed weight=4 block="led 6"
-    export const led6 = new LedClient("led 6")
-    //% fixedInstance whenUsed weight=4 block="led 7"
-    export const led7 = new LedClient("led 7")
-    //% fixedInstance whenUsed weight=4 block="led 8"
-    export const led8 = new LedClient("led 8")
 }
